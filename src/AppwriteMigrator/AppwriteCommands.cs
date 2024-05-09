bf2ef6d2eac36bad8dda6443b42307f1f76e8fac
Matthew Parker <pingu@pinguapps.com>
2024-05-09 14:25:42 +0100

    First instance of using the SDK seems to work

--- src/AppwriteMigrator/AppwriteCommands.cs
+++ src/AppwriteMigrator/AppwriteCommands.cs
@@ -1,15 +1,28 @@
-﻿using Cocona;
+﻿using Appwrite;
+using Appwrite.Services;
+using Cocona;
 
 namespace AppwriteMigrator;
 public class AppwriteCommands
 {
     [Command(Description = "Syncs the settings from your provided project to a local json file.")]
-    public async Task Sync()
+    public async Task Sync([Option('e', Description = "The API endpoint")] string endpoint,
+        [Option("id", ['i'], Description = "the Project ID for the target project")] string projectId,
+        [Option("key", ['k'], Description = "The API Key for the project")] string apiKey)
     {
-        Console.WriteLine("Sync!");
+        Console.WriteLine("# Begin Sync...");
+
+        var client = new Client()
+            .SetEndpoint(endpoint)
+            .SetProject(projectId)
+            .SetKey(apiKey);
+
+        var databases = new Databases(client);
+
+        var dbList = await databases.List();
     }
 
-    [Command(Description = "Migrates the local settings json up to the specified project")]
+    [Command(Description = "Migrates the local settings json up to the specified project.")]
     public async Task Migrate()
     {
         Console.WriteLine("Migrate!");

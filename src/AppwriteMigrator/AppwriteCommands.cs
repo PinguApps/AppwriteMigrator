7f5ef24760cbee61be17a23b9c7a04d35feabeff
Matthew Parker <pingu@pinguapps.com>
2024-05-09 14:49:41 +0100

    Might have already finished sync?

--- src/AppwriteMigrator/AppwriteCommands.cs
+++ src/AppwriteMigrator/AppwriteCommands.cs
@@ -1,14 +1,20 @@
 ﻿using Appwrite;
 using Appwrite.Services;
+using AppwriteMigrator.Models;
 using Cocona;
 
 namespace AppwriteMigrator;
 public class AppwriteCommands
 {
+    // Add filename for json output. Default to something sensible.
+
+
+
     [Command(Description = "Syncs the settings from your provided project to a local json file.")]
     public async Task Sync([Option('e', Description = "The API endpoint")] string endpoint,
         [Option("id", ['i'], Description = "the Project ID for the target project")] string projectId,
-        [Option("key", ['k'], Description = "The API Key for the project")] string apiKey)
+        [Option("key", ['k'], Description = "The API Key for the project")] string apiKey,
+        [Option('f', Description = "The filename to store the schema within")] string fileName = "appwrite-schema.json")
     {
         Console.WriteLine("# Begin Sync...");
 
@@ -17,9 +23,26 @@ public class AppwriteCommands
             .SetProject(projectId)
             .SetKey(apiKey);
 
-        var databases = new Databases(client);
+        var dbClient = new Databases(client);
 
-        var dbList = await databases.List();
+        var databases = await dbClient.List();
+
+        Console.WriteLine($"Found {databases.Databases.Count} database(s)...");
+
+        List<DatabaseExtended> schema = [];
+
+        foreach (var database in databases.Databases)
+        {
+            var collections = await dbClient.ListCollections(database.Id);
+
+            Console.WriteLine($"Database '{database.Name}' contains '{collections.Collections.Count}' collections...");
+
+            var extended = new DatabaseExtended(database, collections.Collections);
+
+            schema.Add(extended);
+        }
+
+        Console.WriteLine("# Sync Complete!");
     }
 
     [Command(Description = "Migrates the local settings json up to the specified project.")]
---
+++ src/AppwriteMigrator/Models/DatabaseExtended.cs
@@ -0,0 +1,17 @@
+﻿using Appwrite.Models;
+
+namespace AppwriteMigrator.Models;
+public class DatabaseExtended : Database
+{
+    public DatabaseExtended(string id, string name, string createdAt, string updatedAt, bool enabled) : base(id, name, createdAt, updatedAt, enabled)
+    {
+
+    }
+
+    public DatabaseExtended(Database database, List<Collection> collections) : base(database.Id, database.Name, database.CreatedAt, database.UpdatedAt, database.Enabled)
+    {
+        Collections = collections;
+    }
+
+    public List<Collection> Collections { get; } = [];
+}

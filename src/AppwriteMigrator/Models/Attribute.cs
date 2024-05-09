4e2d667b5e5931886250888f76a740ca36decb62
Matthew Parker <pingu@pinguapps.com>
2024-05-09 22:44:35 +0100

    Added conversion to concrete types for Attribute

---
+++ src/AppwriteMigrator/Models/Attribute.cs
@@ -0,0 +1,39 @@
+﻿namespace AppwriteMigrator.Models;
+public record Attribute
+{
+    public string Key { get; set; } = default!;
+
+    public string Type { get; set; } = default!;
+
+    public string Status { get; set; } = default!;
+
+    public string Error { get; set; } = default!;
+
+    public bool Required { get; set; }
+
+    public bool Array { get; set; }
+
+    public int? Size { get; set; }
+
+    public object? Default { get; set; }
+
+    public object? Min { get; set; }
+
+    public object? Max { get; set; }
+
+    public string? Format { get; set; }
+
+    public List<object>? Elements { get; set; }
+
+    public string? RelatedCollection { get; set; }
+
+    public string? RelationType { get; set; }
+
+    public bool? TwoWay { get; set; }
+
+    public string? TwoWayKey { get; set; }
+
+    public string? OnDelete { get; set; }
+
+    public string? Side { get; set; }
+}
--- src/AppwriteMigrator/Models/CollectionExtended.cs
+++ src/AppwriteMigrator/Models/CollectionExtended.cs
@@ -1,4 +1,5 @@
 ﻿using Appwrite.Models;
+using Newtonsoft.Json.Linq;
 using System.Text.Json.Serialization;
 
 namespace AppwriteMigrator.Models;
@@ -13,5 +14,52 @@ public class CollectionExtended : Collection
     public CollectionExtended(Collection collection) : base(collection.Id, collection.CreatedAt, collection.UpdatedAt, collection.Permissions, collection.DatabaseId, collection.Name, collection.Enabled, collection.DocumentSecurity, collection.Attributes, collection.Indexes)
     {
 
+    }
+
+    public List<Attribute> ConvertedAttributes => Attributes.Cast<JObject>().Select(ConvertJObjectToAttribute).ToList();
+
+    private Attribute ConvertJObjectToAttribute(JObject? jObject)
+    {
+        if (jObject is null)
+            throw new ArgumentException(null, nameof(jObject));
+
+        return new Attribute
+        {
+            Key = jObject["key"]!.ToString(),
+            Type = jObject["type"]!.ToString(),
+            Status = jObject["status"]!.ToString(),
+            Error = jObject["error"]!.ToString(),
+            Required = jObject["required"]!.ToObject<bool>(),
+            Array = jObject["array"]!.ToObject<bool>(),
+            Size = jObject["size"]?.ToObject<int?>(),
+            Default = jObject["default"]?.ToObject<object>(),
+            Min = GetTypedValue(jObject, "min"),
+            Max = GetTypedValue(jObject, "max"),
+            Format = jObject["format"]?.ToString(),
+            Elements = jObject["elements"]?.ToObject<List<object>>(),
+            RelatedCollection = jObject["relatedCollection"]?.ToString(),
+            RelationType = jObject["relationType"]?.ToString(),
+            TwoWay = jObject["twoWay"]?.ToObject<bool?>(),
+            TwoWayKey = jObject["twoWayKey"]?.ToString(),
+            OnDelete = jObject["onDelete"]?.ToString(),
+            Side = jObject["side"]?.ToString()
+        };
+    }
+
+    private object? GetTypedValue(JObject jObject, string propertyName)
+    {
+        var token = jObject[propertyName];
+
+        if (token == null)
+            return null;
+
+        return (jObject["type"]?.ToString()) switch
+        {
+            "integer" => token.ToObject<int?>(),
+            "double" => token.ToObject<double?>(),
+            "boolean" => token.ToObject<bool?>(),
+            "datetime" => token.ToObject<DateTime?>(),
+            _ => token.ToObject<string?>()
+        };
     }
 }
